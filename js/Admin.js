006f6441b512053358aece888f20524d208d95de
OlwethuB <olwethu.a.bhiya@gmail.com>
2023-06-15 15:47:33 +0200

    Main

---
+++ js/Admin.js
@@ -0,0 +1,151 @@
+// fetch from localstorage
+let products = JSON.parse(localStorage.getItem("Books"));
+
+// adding
+let saveProduct = document.querySelector("#addProduct");
+let adminBody = document.querySelector("#adminContent");
+
+// table
+function display() {
+  try {
+    adminBody.innerHTML = "";
+    if (!products || !products.length) throw "Please add products";
+    products.forEach((product) => {
+      adminBody.innerHTML += `
+                <tr>
+                    <td>${product.id}</td>
+                    <td>${product.name} by ${product.author}</td>
+                    <td>${product.category}</td>
+                    <td>${product.Quantity}</td>
+                    <td>R${product.price}</td>
+                    <td>
+                    <span class="edit" data-bs-toggle="modal" data-bs-target="#editProduct${
+                      product.id
+                    }">Edit</span>
+                    <span class="delete" onclick='deleteProduct(${JSON.stringify(
+                      product
+                    )})'>Delete</span>
+                    </td>
+                        <!-- Modal -->
+                        <div class="modal fade" id="editProduct${
+                          product.id
+                        }" tabindex="-1" aria-labelledby="editProductLabel${
+        product.id
+      }" aria-hidden="true">
+                            <div class="modal-dialog"> 
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h1 class="modal-title fs-5" id="editProductLabel${
+                                    product.id
+                                  }">Edit</h1>
+                                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                  <form class="form g-2">
+                                    <div class="container">
+                                        <input class="form-control" type="text" placeholder="Enter the name of the book" value='${
+                                          product.make
+                                        }' name="Book-name" id="Book-name" required>
+                                        <textarea class="form-control my-2" placeholder="Enter Author name" required name="author" id="author">${
+                                          product.author
+                                        }</textarea>
+                                        <input class="form-control" type="number" placeholder="Enter price" value='${
+                                          product.price
+                                        }' name="amount" id="amount" required>
+                                        <input class="form-control my-2" type="text" placeholder="In what category" value='${
+                                          product.category
+                                        }' name="category" id="category" required>
+                                    </div>
+                                  </form>
+                                </div>
+                                <div class="modal-footer my-2">
+                                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="btnCloseModal">Close</button>
+                                  <button type="button" class="btn btn-success" onclick='new EditProduct(${JSON.stringify(
+                                    product
+                                  )})'>Save changes</button>
+                                </div>
+                              </div>
+                            </div>
+                        </div>        
+                </tr>
+            `;
+    });
+  } catch (e) {
+    alert(e);
+  }
+}
+
+display();
+
+// editing already put items
+class EditProduct {
+  constructor(product) {
+    this.id = product.id;
+    this.name = document.querySelector(`#Book-name${product.id}`).value;
+    this.author = document.querySelector(`#author${product.id}`).value;
+    this.amount = document.querySelector(`#amount${product.id}`).value;
+    this.category = document.querySelector(`#category${product.id}`).value;
+
+    let itemIndex = product.findIndex((data) => {
+      return data.id === product.id;
+    });
+    // Update
+    product[itemIndex] = Object.assign({}, this);
+    localStorage.setItem("Books", JSON.stringify(Product));
+    display();
+    location.reload();
+  }
+}
+
+// Delete
+function deleteProduct(product) {
+  let index = product.findIndex((a) => {
+    return a.id == product.id;
+  });
+  product.splice(index, 1);
+  localStorage.setItem("Books", JSON.stringify(Products));
+  display();
+}
+
+// Adding new product
+saveProduct.addEventListener("click", () => {
+  try {
+    const name = document.querySelector("#addName").value;
+    const author = document.querySelector("#addAuthor").value;
+    const price = document.querySelector("#addPrice").value;
+    const category = document.querySelector("#addCategory").value;
+
+    let id =
+      products.map((item) => item.id).at(-1) >= 1
+        ? products.map((product) => product.id).at(-1)
+        : 0;
+    id++;
+    products.push({
+      id,
+      name,
+      author,
+      price,
+      category,
+    });
+    localStorage.setItem("Books", JSON.stringify(products));
+    display();
+  } catch (e) {
+    alert(e);
+  }
+});
+
+// Sorting
+let sorting = document.querySelector("#sorting");
+let isToggle = false;
+sorting.addEventListener("click", () => {
+  if (!isToggle) {
+    products.sort((a, b) => a.id - b.id);
+    sorting.textContent = "Sorted by asc (ID)";
+    isToggle = true;
+  } else {
+    products.sort((a, b) => b.id - a.id);
+    sorting.textContent = "Sorted by desc (ID)";
+    isToggle = false;
+  }
+  display();
+});
---
+++ js/Products.js
@@ -0,0 +1,475 @@
+// let linkElement = document.createElement("link");
+// linkElement.rel = "stylesheet";
+// linkElement.href = "/Css/Products.css";
+// let display = document.getElementById("books");
+// document.head.appendChild(linkElement);
+
+let Products = JSON.parse(localStorage.getItem('Books')) || [
+  {
+    id:'1',
+    name: "To kill a Mockingbird",
+    image:
+      "/assets/Book Covers/Tokillamockingbird.png",
+    author: "Harper Lee",
+    category: "Novel",
+    price: " 234",
+    quantity: 10,
+  },
+  // Add more product objects here
+  {
+    id:'2',
+    name: "Book1",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "Something",
+    category: "Romance",
+    price: " 926",
+    quantity: 10,
+  },
+  {
+    id:'3',
+    name: "Book1",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Poetry",
+    price: " 926",
+    quantity: 10,
+  },
+  {
+    id:'4',
+    name: "Book1",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Self-help",
+    price: " 926",
+    quantity: 10,
+  },
+  {
+    id:'5',
+    name: "Book1",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Comics",
+    price: " 926",
+    quantity: 10,
+  },
+  {
+    id:'6',
+    name: "Book1",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Biography",
+    price: " 926",
+    quantity: 10,
+  },
+  // Add more product objects here
+  {
+    id:'7',
+    name: "The Alchemist",
+    image:
+      "/assets/Book Covers/theAlchemist.jpg",
+    author: "Paulo Coelho",
+    category: "Novel",
+    price: " 124",
+    quantity: 10,
+  },
+  {
+    id:'8',
+    name: "Book2",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Romance",
+    price: " 926",
+    quantity: 10,
+  },
+  {
+    id:'9',
+    name: "Book2",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Poetry",
+    price: " 926",
+    quantity: 10,
+  },
+  {
+    id:'10',
+    name: "Book2",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Self-help",
+    price: " 926",
+    quantity: 10,
+  },
+  {
+    id:'11',
+    name: "Book2",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Comics",
+    price: " 926",
+    quantity: 10,
+  },
+  {
+    id:'12',
+    name: "Book2",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Biography",
+    price: " 926",
+    quantity: 10,
+  },
+  // Add more product objects here
+  {
+    id:'13',
+    name: "The Hitchhiker’s Guide to the Galaxy",
+    image:
+      "/assets/Book Covers/hitchhikerguide.jpg",
+    author: "Douglas Adams",
+    category: "Novel",
+    price: "250",
+    quantity: 10,
+  },
+  {
+    id:'14',
+    name: "Book3",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Romance",
+    price: " 926",
+    quantity: 10,
+  },
+  {
+    id:'15',
+    name: "Book3",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Poetry",
+    price: " 926",
+    quantity: 10,
+  },
+  {
+    id:'16',
+    name: "Book3",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Self-help",
+    price: " 926",
+    quantity: 10,
+  },
+  {
+    id:'17',
+    name: "Book3",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Comics",
+    price: " 926",
+    quantity: 10,
+  },
+  {
+    id:'18',
+    name: "Book3",
+    image:
+      "https://i.postimg.cc/3RSTXX65/Closed-Book-Clip-Art-Black-and-White.jpg",
+    author: "An Author",
+    category: "Biography",
+    price: " 926",
+    quantity: 10,
+  },
+];
+localStorage.setItem("Books", JSON.stringify(Products));
+
+// Products.forEach((product) => {
+//   display.innerHTML += `
+//     <div class="col">
+//               <img id="photo" src="${product.image}" alt="${product.name}">
+//               <h3>${product.name}</h3>
+//               <h3>${product.author}</h3>
+//               <p> R ${product.price}</p>
+//               <button onclick="addToCart(${product.name})" >Add to cart</button>
+//               </div>`;
+// });
+
+/* This is to display the books*/
+function productsDisplay() {
+  Products.forEach((product) => {
+    // using if statements
+    // Novels
+    if (product.category === "Novel") {
+      let allNovels = document.getElementById("novels");
+      let elementProduct = document.createElement("div");
+      elementProduct.innerHTML += `
+      <div class="product-card col">
+      <img src="${product.image}" alt="${product.name}" class="img-thumbnail">
+      <h3 class="product-name">${product.name}</h3>
+      <p class="author-name">${product.author}</p>
+      <p class="price">R ${product.price}</p>
+      <button  id="addCart" onclick="addClicked(${product.id})" >Add to Cart</button>
+      </div>
+      `;
+      allNovels.appendChild(elementProduct);
+    }
+  });
+
+  // Comics
+  Products.forEach((product) => {
+    // using if statements
+    if (product.category === "Comics") {
+      let allComics = document.getElementById("comics");
+      let elementProduct = document.createElement("div");
+      elementProduct.innerHTML = `
+      <div class="product-card col ">
+      <img src="${product.image}" alt="${product.name}" class="img-thumbnail">
+      <h3 class="product-name">${product.name}</h3>
+      <p class="author-name">${product.author}</p>
+      <p class="price">R ${product.price}</p>
+      <button  id="addCart" onclick="addClicked(${product.id})" >Add to Cart</button>
+      </div>
+      `;
+      allComics.appendChild(elementProduct);
+    }
+  });
+
+  // Self-Help
+  Products.forEach((product) => {
+    // using if statements
+    if (product.category === "Self-help") {
+      let allHelp = document.getElementById("helps");
+      let elementProduct = document.createElement("div");
+      elementProduct.innerHTML = `
+      <div class="product-card col ">
+      <img src="${product.image}" alt="${product.name}" class="img-thumbnail">
+      <h3 class="product-name">${product.name}</h3>
+      <p class="author-name">${product.author}</p>
+      <p class="price">R ${product.price}</p>
+      <button  id="addCart" onclick="addClicked(${product.id})" >Add to Cart</button>
+      </div>
+      `;
+      allHelp.appendChild(elementProduct);
+      // ourAccessories.push(cart);
+      // elementProduct.push(cart);
+    }
+  });
+
+  // Biography
+  Products.forEach((product) => {
+    // using if statements
+    if (product.category === "Biography") {
+      let allBiography = document.getElementById("biography");
+      let elementProduct = document.createElement("div");
+      elementProduct.innerHTML = `
+      <div class="product-card col ">
+      <img src="${product.image}" alt="${product.name}" class="img-thumbnail">
+      <h3 class="product-name">${product.name}</h3>
+      <p class="author-name">${product.author}</p>
+      <p class="price">R ${product.price}</p>
+      <button  id="addCart" onclick="addClicked(${product.id})">Add to Cart</button>
+      </div>
+      `;
+      allBiography.appendChild(elementProduct);
+      // ourAccessories.push(cart);
+      // elementProduct.push(cart);
+    }
+  });
+
+  // Poetry
+  Products.forEach((product) => {
+    // using if statements
+    if (product.category === "Poetry") {
+      let allHelp = document.getElementById("poetry");
+      let elementProduct = document.createElement("div");
+      elementProduct.innerHTML = `
+      <div class="product-card col ">
+      <img src="${product.image}" alt="${product.name}" class="img-thumbnail">
+      <h3 class="product-name">${product.name}</h3>
+      <p class="author-name">${product.author}</p>
+      <p class="price">R ${product.price}</p>
+      <button  id="addCart" onclick="addClicked(${product.id})" >Add to Cart</button>
+      </div>
+      `;
+      allHelp.appendChild(elementProduct);
+      // ourAccessories.push(cart);
+      // elementProduct.push(cart);
+    }
+  });
+
+  // Romance
+  let count = 0
+  Products.forEach((product) => {
+    // using if statements
+    if (product.category === "Romance") {
+      count++
+      console.log(count);
+      let allRomance = document.getElementById("romance");
+      let elementProduct = document.createElement("div");
+      elementProduct.innerHTML = `
+            <div class="product-card col ">
+            <img src="${product.image}" alt="${product.name}" class="img-thumbnail">
+            <h3 class="product-name">${product.name}</h3>
+            <p class="author-name">${product.author}</p>
+            <p class="price">R ${product.price}</p>
+            <button id="addCart" onclick="addClicked(${product.id})">Add to Cart</button>
+            </div>
+            `;
+      allRomance.appendChild(elementProduct);
+      // ourAccessories.push(cart);
+      // elementProduct.push(cart);
+    }
+  });
+}
+// this call displays you nut :[
+
+
+// Adding to cart biz
+/* The end of display display*/
+
+/*Cart display*/
+
+
+/*adding stuff to the cart */
+
+/*// add products to cart
+let addToCart = document.getElementById("addCart");
+let productRow = document.getElementsByClassName("product-row");
+
+for (let i = 0; i < addToCart.length; i++) {
+  button = addToCart[i];
+  button.addEventListener("click", addClicked);
+}
+
+function addClicked(event) {
+  button = event.target;
+  let cartItem = button.parentElement;
+  let itemName = cartItem.getElementsByClassName("product-name")[0].innerText;
+  let itemAuthor = cartItem.getElementsByClassName("author-name")[0].innerText;
+  let price = cartItem.getElementsByClassName("price")[0].innerText;
+
+  let Src = cartItem.getElementsByID("addCart")[0];
+  addItemToCart(itemName, itemAuthor, price);
+  updateCartPrice();
+  console.log();
+}
+
+function addItemToCart(price, itemAuthor, itemName) {
+  let productRow = document.createElement("div");
+  productRow.classList.add("product-row");
+  let productRows = document.getElementsByClassName("product-rows")[0];
+  let cartAdding = document.getElementsByID("addCart");
+
+  for (let i = 0; i < addToCart.length; i++) {
+    if (cartAdding[i] === Src) {
+      alert("This item has already been added to the cart");
+      return;
+    }
+  }
+
+  let cartItems = `
+  <div class="product-row">
+        <div>
+        <h2 class ="cart-name">${itemName}</h2>
+        <h3 class ="cart-author">${itemAuthor}</h3>
+        </div>
+        <span class ="cart-price">${price}</span>
+        <input class="product-quantity" type="number" value="1">
+        <button class="remove">Remove</button>
+        </div>
+        
+      `;
+  productRow.innerHTML = cartItems;
+  productRows.append(productRow);
+  productRow
+    .getElementsByClassName("remove")[0]
+    .addEventListener("click", removeItem);
+  productRow
+    .getElementsByClassName("product-quantity")[0]
+    .addEventListener("change", changeQuantity);
+  updateCartPrice();
+}
+
+// Remove products from cart
+let removeBtn = document.getElementsByClassName("remove");
+for (var i = 0; i < removeBtn.length; i++) {
+  button = removeBtn[i];
+  button.addEventListener("click", removeItem);
+}
+
+function removeItem(event) {
+  btnClicked = event.target;
+  btnClicked.parentElement.parentElement.remove();
+  updateCartPrice();
+}*/
+
+// 
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+// let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addClicked(productId) {
+  let cartContainer = document.getElementById("cartBody");
+  let product = Products.find((product) => product.id == productId);
+  if (product) {
+    cart.push(product);
+    localStorage.setItem('cart', JSON.stringify(cart))
+    updateCart();
+  }
+    
+}
+
+function deleteFromCart(index) {
+  let deletedProduct = cart.splice(index, 1)[0];
+  localStorage.setItem('cart', JSON.stringify(cart))
+  deletedProduct.quantity++;
+  updateCart();
+}
+updateCart()
+function updateCart() {
+  let cartContainer = document.getElementById("cartBody");
+  // localStorage.setItem("products", JSON.stringify(cart));
+  // localStorage.setItem("cart", JSON.stringify(cart));
+  cartContainer.innerHTML = "";
+  cart.forEach((product, index) => {
+      let cartProduct = document.createElement("div");
+      cartProduct.innerHTML = `
+      <span>${product.name}</span>
+      <span>${product.author}</span>
+      <span>${product.price}</span>
+      <input type="number" placeholder="1" min="1" width="50px" height="40px">
+      <p>Total ${product.price}</p>
+      <button onclick="deleteFromCart(${index})" class="delbtn">Delete</button>`;
+      cartContainer.appendChild(cartProduct);
+  });
+    calculateTotal();
+}
+
+productsDisplay();
+
+function calculateTotal() {
+    let totalElement = document.getElementById("total");
+    let total = 0 
+    cart.forEach(item => {
+      total +=  eval(item.price)
+    })
+
+    totalElement.textContent = `${total}`;
+}
+
+function clearCheckoutCart() {
+    let modalFooter = document.querySelector(".modal-footer");
+    modalFooter.innerHTML = `
+    <h4>Thank You! Your Order Has Been Processed!</h4>`;
+    cart = [];
+    updateCart();
+}
+
